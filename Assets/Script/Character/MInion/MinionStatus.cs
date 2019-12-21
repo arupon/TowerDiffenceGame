ff681b3c2e1cf72a62935c27ba3dee771e3582a5
arupon <arupon2525@gmail.com>
2019-12-21 19:25:57 +0900

    初期追加

---
+++ Assets/Script/Character/MInion/MinionStatus.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinionStatus : MonoBehaviour
+{
+    [SerializeField]
+    private int hp;
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        //Debug.Log("MinionStatus StartFunction Start");
+        //Debug.Log("MinionStatus StartFunction Finish");
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        Debug.Log("MinionStatus UpdateFunction Start");
+
+        if(hp <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+
+        Debug.Log("MinionStatus UpdateFunction Finish");
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "CannonBall")
+        {
+            hp = hp - 500;
+        }
+    }
+}
