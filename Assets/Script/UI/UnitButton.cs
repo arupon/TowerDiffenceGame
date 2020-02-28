2f5c218ff48dc017ca578567a77acc4782a1e215
arupon <arupon2525@gmail.com>
2020-02-29 03:44:56 +0900

    no message

---
+++ Assets/Script/UI/UnitButton.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class UnitButton: MonoBehaviour
+{
+    [SerializeField]
+    private GameObject controller;
+    [SerializeField]
+    private GameObject unit;
+    
+    public void OnClick()
+    {
+        Debug.Log("Click");
+
+        controller.GetComponent<UnitSelectController>().SetObject(Instantiate(unit, Camera.main.transform.position - new Vector3(0.0f,0.0f,-100.0f), Quaternion.identity));
+    }
+}
